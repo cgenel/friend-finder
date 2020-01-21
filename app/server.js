c132a6ace054d9e6bb08e506769a7257e1f6a670
cgenel <genel661@gmail.com>
2020-01-20 18:49:04 -0800

    created server.js installed packages

---
+++ app/server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var jsonParser = bodyParser.json()
+
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+
+// parsing custom json types as json
+app.use(bodyParser.json({type: "application/*+json"}))
+
+// parsing into a buffer
+app.use(bodyParser.raw({type: "application/vnd.custom-type"}))
+
+// parsing html into a string
+app.use(bodyParser.text({type: "text/html"}))
+
+app.listen(PORT, function() {
+  console.log("app is listening on PORT: " + PORT);
+});
